ef1d3c84338a3bff61fe73ae8318a2b875b6aebc
arduinobymyself <arduinobymyself@gmail.com>
2022-08-15 11:25:38 -0300

    subindo modificações no arquivo TipoVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,2 +1,24 @@
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        //declaração de variáveis
+        int idadePai; // declaração sem atribuição
+        int anoFabricacao = 2022; // declaração com atribuição
+        double salarioMinimo = 2500.00;
+
+        byte idade = 123;
+        short ano = 2022;
+        int cep = 18050000; //se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 12345678910; //se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14f;
+        double salario = 1275.33;
+
+        //casting - conversão entre tipos
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; //um short cabe em um int, sem problemas
+        short numeroCurto2 = numeroNormal; //apesar de numero normal ser menor que o limite de um short,
+        // em algum momento ele poderá ser maior e portanto não é possível fazer o casting
+
+
+
+    }
 }
