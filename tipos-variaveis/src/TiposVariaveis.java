4146e969d70eec28d6e73ebef73b5a6b5f6ca451
arduinobymyself <arduinobymyself@gmail.com>
2022-08-15 12:49:46 -0300

    subindo modificações no arquivo TipoVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -19,7 +19,27 @@ public class TiposVariaveis {
         // em algum momento ele poderá ser maior e portanto não é possível fazer o casting
         // e isso provavelmente gerará um erro.
 
+
+        /*
+        esta linha é considerada como declaração de variável iniciamos a existência
+        da variável numero com valor 5, regra: tipo + nome + valor
+         */
+        int numero = 5;
+
+        /*
+        na linha abaixo iremos alterar o valor da variável para 10, observe que o tipo
+        não é mais necessário, pois a vairável já foi declarada anteriormente
+         */
+        numero = 10;
+
+        /*
+        ao usar a palavra reservada final, você determina que jamais
+        esta vairável poderá obter outro valor;
+        isso é considerado uma constante em linguagem Java
+         */
         final double VALOR_DE_PI = 3.1415927; //constantes devem ser declaradas como final
+
+        VALOR_DE_PI = 3.14; // isso gerará um erro pois uma constante não pode ter seu valor alterado
 
     }
 }
