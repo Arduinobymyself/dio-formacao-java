f83925eb62e454a6f123d53884b20937f7e09ccd
arduinobymyself <arduinobymyself@gmail.com>
2022-08-15 16:35:06 -0300

    subindo modificações no arquivo Operadores.java

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,9 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Operadores {
     public static void main(String[] args) {
 
-        //operadores de atribuição (=)
+        //OPERADORES DE ATRIBUIÇÃO (=)
         String nome = "Marcelo";
         int idade = 51;
         double peso = 73.2;
@@ -10,7 +12,7 @@ public class Operadores {
         //Date dataNascimento = new Date(); // o tipo Date necessita ser definido
 
 
-        //Operadores Aritméticos
+        //OPERADORES ARITMÉTICOS
         // +, -, * e /
         double soma = 10.5 + 15.7;
         int subtrtacao = 113 - 25;
@@ -42,7 +44,7 @@ public class Operadores {
         System.out.println(concatenacao); // 1111
 
 
-        //Operadores unários
+        //OPERADORES UNÁRIOS
         int numero = 5;
 
         //Imprimendo o número negativo
@@ -73,6 +75,49 @@ public class Operadores {
 
         String resultadoOperadorTernario = (a == b) ? "Verdadeiro, a = b" : "Falso, a != b";
         System.out.println(resultadoOperadorTernario);
+
+        //OPERADORES RELACIONAIS
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.println("numero1 maior que numero2");
+        if(numero1 < numero2)
+            System.out.println("numero1 menor que numero2"); // será impresso
+        if(numero1 >= numero2)
+            System.out.println("numero1 maior ou igual que numero2");
+        if(numero1 <= numero2)
+            System.out.println("numero1 menor ou igual que numero2"); // será impresso
+        if(numero1 != numero2)
+            System.out.println("numero1 diferente de numero2"); // será impresso
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+        System.out.println(nome1 == nome2); // true
+        String nome3 = new String("JAVA");
+        System.out.println(nome1 == nome3); // false
+        String nome4 = nome3;
+        System.out.println(nome3 == nome4); // true
+
+        //usando equals
+        System.out.println(nome1.equals(nome2)); // true
+        System.out.println(nome3.equals(nome3)); // true
+        System.out.println(nome3.equals(nome4)); // true
+
+
+        numero1 = 130;
+        numero2 = 130;
+        System.out.println(numero1 == numero2); // true
+
+
+        //tratando como objeto
+        Integer numero3 = 130;
+        Integer numero4 = 130;
+        System.out.println(numero3 == numero4); // false
+        // a razão pela qual o resultado é false, é devido que o Java trata os valores
+        // como objetos a partir da criação da variável
+        // quando queremos comparar objetos, usamos o método equals
+        System.out.println(numero1.equals(numero2));
 
     }
 }
